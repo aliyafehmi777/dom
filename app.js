54faa6ab15aea85b1c8e20193e8677c4e1c991d6
A.Fehmi <Aliyafehmi@uconnect.pk>
2019-01-02 11:19:55 +0500

    add Dom

---
+++ app.js
@@ -0,0 +1,15 @@
+let doc;
+doc = document;
+doc = document.all;
+doc = document.all[1];
+doc = document.all.length;
+
+doc = document.URL;
+doc = document.doctype;
+doc = document.domain;
+doc = document.contentType;
+
+doc = document.links;
+doc = document.scripts;
+
+console.log(doc)
